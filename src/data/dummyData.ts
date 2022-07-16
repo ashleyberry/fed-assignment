677363df02334e733d1dd3cf071e8807a06b738d
ashleyberry <ashleywberry@gmail.com>
2022-07-16 12:13:22 -0500

    Create dummyData.ts

---
+++ src/data/dummyData.ts
@@ -0,0 +1,30 @@
+import { HeaderMenuItem } from "../areas/SecondaryHeaderAppBar";
+
+export const secondaryMenuItems: HeaderMenuItem[] = [
+  {
+    name: "Impact",
+    subMenuItemList: [
+      "Annual Report",
+      "BrandLab Stories",
+      "State of the Industry",
+    ],
+  },
+  { name: "Programs", subMenuItemList: ["Item 1", "Item 2", "Item 3"] },
+  {
+    name: "Join the Movement",
+    subMenuItemList: ["Item 1", "Item 2", "Item 3"],
+  },
+  { name: "Job Board", subMenuItemList: ["Item 1", "Item 2", "Item 3"] },
+];
+
+export const mainMenuItems: HeaderMenuItem[] = [
+  {
+    name: "About",
+  },
+  {
+    name: "News",
+  },
+  {
+    name: "Contact",
+  },
+];
