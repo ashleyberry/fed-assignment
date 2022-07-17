b7eba132d18495bfdd241e8bf19ebff04a301ebe
ashleyberry <ashleywberry@gmail.com>
2022-07-16 19:28:04 -0500

    adds margin bottom for ui styling

--- src/areas/Layout.tsx
+++ src/areas/Layout.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { HeaderAppBar } from "./HeaderAppBar";
 import { SecondaryHeaderAppBar } from "./SecondaryHeaderAppBar";
+import { Box } from "@mui/material";
 
 export const Layout = () => {
   return (
-    <>
+    //  TODO: remove this. it's just for UI styling to see bottom of absolutely positioned paper
+    <Box marginBottom={50}>
       <HeaderAppBar />
       <SecondaryHeaderAppBar />
-    </>
+    </Box>
   );
 };
