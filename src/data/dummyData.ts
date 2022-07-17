dd2c975df79d0f800becf436e017c81becf5aea2
ashleyberry <ashleywberry@gmail.com>
2022-07-16 19:28:35 -0500

    creates our programs item type and dummy data for page

--- src/data/dummyData.ts
+++ src/data/dummyData.ts
@@ -1,6 +1,58 @@
-import { HeaderMenuItem } from "../areas/SecondaryHeaderAppBar";
+import classroom from "./../assets/classroom.png";
+import internship from "./../assets/internship.png";
+import connect from "./../assets/connect.png";
+import fearless from "./../assets/fearless.png";
 
-export const secondaryMenuItems: HeaderMenuItem[] = [
+export type HeaderMenuItemType = {
+  name: string;
+  subMenuItemList?: string[];
+};
+
+export type OurProgramsItemType = {
+  imageUrl: string;
+  name: string;
+  description: string;
+};
+
+export const ourProgramsItems: OurProgramsItemType[] = [
+  {
+    imageUrl: classroom,
+    name: "Classroom",
+    description:
+      "Eh discovered his career interests through our classroom experiences.",
+  },
+  {
+    imageUrl: internship,
+    name: "Internship",
+    description:
+      "Ingrid worked with a local agency through our partner internship program.",
+  },
+  {
+    imageUrl: connect,
+    name: "Connect",
+    description:
+      "Travon made valuable professional connections through our connect program.",
+  },
+  {
+    imageUrl: fearless,
+    name: "Fearless",
+    description: "Get involved and learn more at our Fearless Conversations.",
+  },
+];
+
+export const mainMenuItems: HeaderMenuItemType[] = [
+  {
+    name: "About",
+  },
+  {
+    name: "News",
+  },
+  {
+    name: "Contact",
+  },
+];
+
+export const secondaryMenuItems: HeaderMenuItemType[] = [
   {
     name: "Impact",
     subMenuItemList: [
@@ -15,16 +67,4 @@ export const secondaryMenuItems: HeaderMenuItem[] = [
     subMenuItemList: ["Item 1", "Item 2", "Item 3"],
   },
   { name: "Job Board", subMenuItemList: ["Item 1", "Item 2", "Item 3"] },
-];
-
-export const mainMenuItems: HeaderMenuItem[] = [
-  {
-    name: "About",
-  },
-  {
-    name: "News",
-  },
-  {
-    name: "Contact",
-  },
 ];
