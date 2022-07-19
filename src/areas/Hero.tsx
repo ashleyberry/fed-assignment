551bb474dff1f0aec17e46707313b274c117c638
ashleyberry <ashleywberry@gmail.com>
2022-07-19 00:05:34 -0500

    updates width of hero content

--- src/areas/Hero.tsx
+++ src/areas/Hero.tsx
@@ -1,40 +1,26 @@
 import React from "react";
 import { Button, Grid, Typography } from "@mui/material";
-import { styled } from "@mui/system";
-import backgroundImage from "../assets/background-image.png";
 
 export const Hero = () => {
   return (
-    <StyledGrid justifyContent={"center"}>
-      <Grid item marginTop={25}>
-        <Typography
-          marginX={"auto"}
-          width={"50%"}
-          textAlign={"center"}
-          color={(theme) => theme.palette.text.secondary}
-          variant="h4"
-          component="h1"
-        >
-          We believe in a future industry that reaches new heights through the
-          creativity of people of any race or socioeconomic background.
-        </Typography>
-      </Grid>
+    <Grid item marginTop={25}>
+      <Typography
+        marginX={"auto"}
+        width={"80%"}
+        textAlign={"center"}
+        color={(theme) => theme.palette.text.secondary}
+        variant="h2"
+        component="h1"
+      >
+        We believe in a future industry that reaches new heights through the
+        creativity of people of any race or socioeconomic background.
+      </Typography>
 
       <Grid item marginTop={5} textAlign={"center"}>
         <Button onClick={() => console.log("clicked button")}>
           Learn More
         </Button>
       </Grid>
-    </StyledGrid>
+    </Grid>
   );
 };
-
-export const StyledGrid = styled(Grid)`
-  position: absolute;
-  top: 4rem;
-  background-image: url(${backgroundImage});
-  background-repeat: no-repeat;
-  height: 900px;
-  background-size: contain;
-  justify-content: center;
-`;
